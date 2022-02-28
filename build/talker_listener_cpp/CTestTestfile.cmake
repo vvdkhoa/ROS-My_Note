b85268a17a5dc5b2003220be1fe62b8561cda56d
vvdkhoa <vovandangkhoa@gmail.com>
2022-02-28 09:15:59 +0900

    custom git ignore

--- build/talker_listener_cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vvdkhoa/catkin_ws/src/talker_listener_cpp
-# Build directory: /home/vvdkhoa/catkin_ws/build/talker_listener_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/talker_listener_py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vvdkhoa/catkin_ws/src/talker_listener_py
-# Build directory: /home/vvdkhoa/catkin_ws/build/talker_listener_py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
